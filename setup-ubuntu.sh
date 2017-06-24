5d43bcd47d13aca945ac47a3bd0769eef04e005d
administrator <administrator@rtointernaldev.com>
2017-06-23 19:21:21 -0500

    Improve Ubuntu script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,10 +6,26 @@ if [ ! -d ~/BashScripts ]; then
 else
 
 	if [ -f ~/.bash_aliases  ]; then
-		printf "Moving your current .bash_aliases to a backup file..."
-		sudo mv ~/.bash_aliases ~/.bash_aliases_BACKUP
+		printf "%-100s" "Moving your current .bash_aliases to a backup file..."
+		mv ~/.bash_aliases ~/.bash_aliases_BACKUP
 		printf "done\n"
 	fi
 
+	if [ -f ~/.vimrc  ]; then
+		printf "%-100s" "Moving your current .vimrc to a backup file..."
+		mv ~/.vimrc ~/.vimrc_BACKUP
+		printf "done\n"
+	fi
+
+	printf "%-100s" "Setting up new .bash_aliases..."
+	ln -s ~/BashScripts/ubuntu/bash_aliases ~/.bash_aliases
+	source ~/.bash_aliases
+	printf "done\n"
+
+	printf "%-100s" "Setting up new .vimrc..."
+	ln -s ~/BashScripts/ubuntu/vimrc ~/.vimrc
+	printf "done\n"	
+
+	printf "You are good to go! Happy bashing!\n"
 
 fi
